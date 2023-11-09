a0f61b9dfbec652bcc7f62eb579d8df07e872f52
Dana Holubová <danmich@email.cz>
2023-11-09 18:05:06 +0100

    Spuštění testu.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+document.querySelector(".question").textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon."
 
+const user_answer = prompt("Odpovězte na otázku: ")
+
+document.querySelector(".answer__text").textContent = user_answer
+
+const answer = document.querySelector(".answer")
+
+if (user_answer === "Bell") { answer.classList.add("answer--correct") }
+else { answer.classList.add("answer--wrong") }
